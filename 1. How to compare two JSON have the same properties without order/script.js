d0d46e7e3cea983de62451a97b2b176a9c6de00d
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-03-13 16:35:20 +0000

    Add files via upload

--- 1. How to compare two JSON have the same properties without order/script.js
+++ 1. How to compare two JSON have the same properties without order/script.js
@@ -1,4 +1,39 @@
-let obj1 = { name: "Person 1", age:5 };
-let obj2 = { age:5, name: "Person 1" };
-let a=JSON.stringify(obj1)===JSON.stringify(obj2)
-console.log(a)
+   
+//given json data iterate with for,for in,for of and forEach loops respectively.
+let employee=[{"Name":"vino",
+         "age":"23",
+         "email":" vinogurunath@gmail.com",
+         "salary":"75k"},
+         
+         {"Name":"sathiya",
+         "age":"21",
+         "email":"sathiya@gmail.com",
+         "salary":"50k"},
+         
+         {"Name":"chandru",
+         "age":"22",
+         "email":"chandru@gmail.com",
+         "salary":"40k"}];
+
+
+//for loop   
+
+for(i=0;i<employee.length;i++){
+   console.log([i]);          
+} ;
+console.log("...............");
+//for in loop
+
+ for( let x in employee){
+     console.log(employee[x]);
+ } ;       
+ console.log("..............");
+//for of loop
+
+ for(let a of employee){
+     console.log(a);
+ };
+ console.log("..............");
+// //for Each loop
+
+employee.forEach(element => console.log(element));
