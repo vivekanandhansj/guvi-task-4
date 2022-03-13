fac64f6796f97353865c40d753b18b4d25729b31
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-03-13 16:35:45 +0000

    Add files via upload

--- 2. Use the rest countries API url and display all the country flags in console./script.js
+++ 2. Use the rest countries API url and display all the country flags in console./script.js
@@ -1,18 +1,39 @@
-const request = new XMLHttpRequest()
-request.open("GET","https://restcountries.com/v3.1/all")
-request.send()
+   
+//given json data iterate with for,for in,for of and forEach loops respectively.
+let employee=[{"Name":"vino",
+         "age":"23",
+         "email":" vinogurunath@gmail.com",
+         "salary":"75k"},
+         
+         {"Name":"sathiya",
+         "age":"21",
+         "email":"sathiya@gmail.com",
+         "salary":"50k"},
+         
+         {"Name":"chandru",
+         "age":"22",
+         "email":"chandru@gmail.com",
+         "salary":"40k"}];
 
-request.onload = function()
-{
-if(request.status==200)
-{
-    let c = JSON.parse(request.responseText)
-    c.map((s)=>{
-        console.log(s.flags.svg);
-    })
-}
-else{
-    request.onerror=function(){
-        console.log("error")
-    }
-}}
+
+//for loop   
+
+for(i=0;i<employee.length;i++){
+   console.log([i]);          
+} ;
+console.log("...............");
+//for in loop
+
+ for( let x in employee){
+     console.log(employee[x]);
+ } ;       
+ console.log("..............");
+//for of loop
+
+ for(let a of employee){
+     console.log(a);
+ };
+ console.log("..............");
+// //for Each loop
+
+employee.forEach(element => console.log(element));
